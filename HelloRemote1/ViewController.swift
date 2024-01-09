30b24d251d79e78c08ced6b3fc8647f1a53e8e1e
shenfive <shenfive@gmail.com>
2024-01-09 18:55:26 +0800

    Initial Commit

---
+++ HelloRemote1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloRemote1
+//
+//  Created by Danny Shen on 2024/1/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
